28c809997d069fe5ed1724225b97e0c45d945dde
Webber Huang <webberhuang@qnap.com>
2022-01-21 12:37:03 +0800

    Update pipelineProtobuf to handle complex structure in private
    Update pipelineProtobuf to handle complex structure in private,
    which is close the situation in data pipeline application

--- pipelineProtobuf/main.go
+++ pipelineProtobuf/main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"reflect"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
 )
 
-//type roamingConfig map[string]*ConnectConfig
+func myHookFunc() mapstructure.DecodeHookFunc {
+	return func(
+		f reflect.Type,
+		t reflect.Type,
+		data interface{}) (interface{}, error) {
+
+		if t != reflect.TypeOf([]byte{}) {
+			return data, nil
+		}
+
+		// fmt.Printf("f %v\n", f)
+		// fmt.Printf("t %v\n", t)
+		// fmt.Printf("data %v\n", data)
+
+		bytes, err := yaml.Marshal(data)
+		if err != nil {
+			log.Fatalf("marshal data fail %v", err)
+		}
+		//log.Printf("bytes %v", string(bytes))
+
+		return bytes, nil
+	}
+}
+
+type Pool struct {
+	Value int
+	Sizes []int
+}
+
+type Private struct {
+	Pools []Pool
+}
 
 func main() {
 	viper.SetConfigName("config")
@@ -16,17 +49,21 @@ func main() {
 
 	err := viper.ReadInConfig()
 	if err != nil {
-		log.Fatal("read config failed: %v", err)
+		log.Fatalf("read config failed: %v", err)
 	}
+
+	optDecode := viper.DecodeHook(mapstructure.ComposeDecodeHookFunc(myHookFunc()))
 
 	var rc RoamingConfig
-	viper.Unmarshal(&rc)
-
-	for k, v := range rc.GetConnects() {
-		fmt.Printf("\n---- stage:%v ----\n", k)
-		fmt.Printf("initiator:%v\n", v.GetInitiator())
-		if v.GetTrigger() != nil {
-			fmt.Printf("trigger:%v\n", v.GetTrigger())
-		}
+	if err := viper.Unmarshal(&rc, optDecode); err != nil {
+		log.Fatalf("viper unmarshal failed %v ", err)
 	}
+	log.Printf("rc:%v\n", rc)
+
+	var p Private
+	bs := rc.GetPipeline().GetServices()[1].GetPrivate()
+	if err := yaml.Unmarshal(bs, &p); err != nil {
+		log.Fatalf("Unmarshal private failed %v\n", err)
+	}
+	log.Printf("p:%v\n", p)
 }
